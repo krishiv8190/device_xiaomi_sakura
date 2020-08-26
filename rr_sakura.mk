59c7a9dcefa963ecd2d3ede5ff2d255d9ba78385
krishiv8190 <47680902+krishiv8190@users.noreply.github.com>
2020-08-26 01:49:17 -0700

    Add MiuiCamera Path

--- rr_sakura.mk
+++ rr_sakura.mk
@@ -16,7 +16,10 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)\
+
+# Inherit from MiuiCamera
+$(call inherit-product, vendor/MiuiCamera/config.mk)
 
 # Inherit from mido device
 $(call inherit-product, device/xiaomi/sakura/device.mk)
